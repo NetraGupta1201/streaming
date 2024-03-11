2bca3159aaecc3f18bf2d6d75068ac0825ce5319
NetraGupta1201 <netragupta1201@gmail.com>
2024-03-11 23:31:56 +0530

    upload changed + firebase

--- pages/firebase.js
+++ pages/firebase.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
-
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyA_m0IdrYGulFWDh6TRO4BH-WRTT45mMVA",
   authDomain: "streaming-fe5c6.firebaseapp.com",
@@ -15,8 +14,6 @@ const firebaseConfig = {
   appId: "1:283055978472:web:4b4a6a9d7a5489e069565d",
 };
 
-if (!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-}
-
-export default firebase
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
