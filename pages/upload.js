47f5d0e72e7395256fac743bcfa5353eea2373a5
NetraGupta1201 <netragupta1201@gmail.com>
2024-03-16 15:59:59 +0530

    accept img & vid input

--- pages/upload.js
+++ pages/upload.js
@@ -4,41 +4,41 @@ import React, { useState } from "react";
 import uploadFileToFirebase from "./api/upload";
 
 const UploadComponent = () => {
-  const [imgUrl, setImgUrl] = useState(null);
-  const [progresspercent, setProgresspercent] = useState(0);
+  const [fileUrl, setFileUrl] = useState(null);
+  const [progressPercent, setProgressPercent] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const file = e.target[0]?.files[0];
     if (!file) return;
-    uploadFileToFirebase(file, setImgUrl, setProgresspercent);
+    uploadFileToFirebase(file, setFileUrl, setProgressPercent);
   };
 
   return (
     <div className="container mx-auto px-4 md:px-10">
-      <form onSubmit={handleSubmit} className='form' encType="multipart/form-data"> {/* Add encType attribute */}
+      <form onSubmit={handleSubmit} className="form" encType="multipart/form-data">
         <div className="flex flex-col space-y-3">
           <label
-            htmlFor="video-upload"
+            htmlFor="file-upload"
             className="my-4 font-medium font-bold text-2xl"
           >
-            Upload a Video:
+            Upload a File:
           </label>
           <input
             type="file"
-            id="video-upload" /* Add id attribute */
-            accept="video/*"
+            id="file-upload"
+            accept="video/*,image/*" // Accept both video and image file types
             className="border border-gray-300 bg-white px-2 py-1 rounded-lg text-sm focus:outline-none focus:border-gray-400"
           />
           <button
             type="submit"
             className="bg-black w-fit text-white px-4 py-2 rounded-full shadow-md hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 transition duration-200 ease-in-out"
           >
-            Upload Video
+            Upload File
           </button>
         </div>
       </form>
-      {!imgUrl && (
+      {!fileUrl && (
         <div
           className="outerbar mx-auto mt-6 px-8 md:px-32"
           style={{ maxWidth: "500px" }}
@@ -46,18 +46,15 @@ const UploadComponent = () => {
           <div className="relative h-2 rounded-full bg-gray-200">
             <div
               className="absolute left-0 top-0 h-full bg-blue-500 rounded-full"
-              style={{ width: `${progresspercent}%` }}
+              style={{ width: `${progressPercent}%` }}
             ></div>
           </div>
-          <div className="text-center mt-2">{progresspercent}%</div>
+          <div className="text-center mt-2">{progressPercent}%</div>
         </div>
       )}
-      {imgUrl && (
-        <div
-          className="mx-auto mt-6 px-8 md:px-32"
-          style={{ maxWidth: "500px" }}
-        >
-          <img src={imgUrl} alt="uploaded file" height={200} />
+      {fileUrl && (
+        <div className="mx-auto mt-6 px-8 md:px-32" style={{ maxWidth: "500px" }}>
+          <img src={fileUrl} alt="uploaded file" height={200} />
         </div>
       )}
     </div>
