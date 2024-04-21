6c94a76f8c9f4d7f48d277b79969683c9bf9860b
NetraGupta1201 <netragupta1201@gmail.com>
2024-04-21 15:15:55 +0530

    fix video player

--- pages/watch.js
+++ pages/watch.js
@@ -1,23 +1,44 @@
 import { useRouter } from 'next/router';
+import { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
 const Watch = () => {
   const router = useRouter();
   const { url } = router.query;
+  const videoRef = useRef(null);
 
-  const isVideo = url && /\.(mp4|mov|avi|wmv|flv)/i.test(url);
+  useEffect(() => {
+    if (url) {
+      const videoPlayer = videojs(videoRef.current, {
+        controls: true,
+        sources: [{ src: url, type: 'video/mp4' }],
+        fluid: true // Makes the video player responsive
+      });
+
+      return () => {
+        // Clean up the video player when component unmounts
+        if (videoPlayer) {
+          videoPlayer.dispose();
+        }
+      };
+    }
+  }, [url]);
 
   return (
     <div className="container mx-auto px-4 md:px-10 pt-20 flex justify-center items-center v-screen">
-      {isVideo ? (
-        <video controls className="max-w-full max-h-full">
-          <source src={url} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      ) : (
-        <img src={url} alt="Image" className="max-w-full max-h-full" />
-      )}
+      <div
+        data-vjs-player
+        style={{ maxWidth: '100vw', maxHeight: 'calc(100vh - 100px)' }} // Adjusted for potential header/footer
+      >
+        <video
+          ref={videoRef}
+          className="video-js"
+          style={{ maxWidth: '100%', maxHeight: '100%' }}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default Watch;
